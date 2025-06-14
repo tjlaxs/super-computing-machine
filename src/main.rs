109287cf72f8f98c9662ff738dd454f637205bd5
Tero Laxström <tlax@neonpeons.com>
2025-06-14 13:34:33 +0300

    Getting langs for repos

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ async fn main() {
     let client = new_client().expect("no client");
     let repos = client.user_repos("tjlaxs").await.expect("failing");
     for r in repos.repos {
-        thread::sleep(time::Duration::from_millis(10));
+        thread::sleep(time::Duration::from_millis(50));
+        let languages = client
+            .repo_languages("tjlaxs", &r.name)
+            .await
+            .expect("repo langs");
         println!("{}", r.name);
+        for (lang, num) in languages.languages {
+            println!("  {}: {}", lang, num)
+        }
     }
 }
